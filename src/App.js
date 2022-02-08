91fdf27d4935526ffabf9f6f546b8f8f359786e2
take917 <35625934+take917@users.noreply.github.com>
2022-02-08 12:35:30 +0900

    下の階層のページ管理 Routeを使い、子ページ、孫ページの遷移を実施

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Home } from "./Home";
 import { Page1 } from "./Page1";
 import { Page2 } from "./Page2";
 import { Switch } from "react-router-dom";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 
 export default function App() {
   return (
@@ -17,12 +19,26 @@ export default function App() {
         <Link to="/Page2">Page2</Link>
       </div>
       <Switch>
-        <Route exact path="/Home">
+        <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/Page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/Page1"
+          render={() => (
+            <Switch>
+              <Route exact path="/page1">
+                <Page1 />
+              </Route>
+              <Route path="/page1/detailA">
+                <Page1DetailA />
+              </Route>
+              <Route path="/page1/detailB">
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
+
         <Route path="/Page2">
           <Page2 />
         </Route>
