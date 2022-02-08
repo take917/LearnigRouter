6cad9c286d9c83ff32de44c77ad1e72102cc828e
take917 <35625934+take917@users.noreply.github.com>
2022-02-08 16:05:10 +0900

    urlパラメーター urlパラメータの実装をUrlParameterへ

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 import { Home } from "../Home";
-import { Page1 } from "../Page1";
-import { Page1DetailA } from "../Page1DetailA";
-import { Page1DetailB } from "../Page1DetailB";
-import { Page2 } from "../Page2";
+import { page1Routes } from "./Page1Routes";
+import { page2Routes } from "./Page2Routes";
 export const Router = () => {
   return (
     <Switch>
@@ -14,22 +12,35 @@ export const Router = () => {
         path="/Page1"
         render={({ match: { url } }) => (
           <Switch>
-            <Route exact path={url}>
-              <Page1 />
-            </Route>
-            <Route path={`${url}/detailA`}>
-              <Page1DetailA />
-            </Route>
-            <Route path={`${url}/detailB`}>
-              <Page1DetailB />
-            </Route>
+            {page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
           </Switch>
         )}
       />
 
-      <Route path="/Page2">
-        <Page2 />
-      </Route>
+      <Route
+        path="/Page2"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page2Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
     </Switch>
   );
 };
