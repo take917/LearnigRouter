5a0a97813e3c8e9f9229ad0d3391b7a876095231
take917 <35625934+take917@users.noreply.github.com>
2022-02-08 14:08:44 +0900

    Routerコンポーネントを分割 Routerの内容を別ファイルに

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import { BrowserRouter, Link, Route } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
 import "./styles.css";
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page2 } from "./Page2";
-import { Switch } from "react-router-dom";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
@@ -18,31 +13,7 @@ export default function App() {
         <br />
         <Link to="/Page2">Page2</Link>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/Page1"
-          render={({ match: { url } }) => (
-            <Switch>
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route path={`${url}/detailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route path={`${url}/detailB`}>
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        />
-
-        <Route path="/Page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
---
+++ src/router/Router.jsx
@@ -0,0 +1,35 @@
+import { Route, Switch } from "react-router-dom";
+import { Home } from "../Home";
+import { Page1 } from "../Page1";
+import { Page1DetailA } from "../Page1DetailA";
+import { Page1DetailB } from "../Page1DetailB";
+import { Page2 } from "../Page2";
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/Page1"
+        render={({ match: { url } }) => (
+          <Switch>
+            <Route exact path={url}>
+              <Page1 />
+            </Route>
+            <Route path={`${url}/detailA`}>
+              <Page1DetailA />
+            </Route>
+            <Route path={`${url}/detailB`}>
+              <Page1DetailB />
+            </Route>
+          </Switch>
+        )}
+      />
+
+      <Route path="/Page2">
+        <Page2 />
+      </Route>
+    </Switch>
+  );
+};
